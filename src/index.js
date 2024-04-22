6f8cf678d6e74516ecdc056ac2fe7e2ebd3f4d95
Joanafgg <joanafgg13@gmail.com>
2024-04-22 01:06:04 +0100

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-function generateRecipe(event) {
-    event.preventDefault();
+/** @format */
 
-    new Typewriter("#recipe", {
-      strings: "Carrot",
-        autoStart: true,
-        delay: 20,
-      cursor: null,
-    });
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 2,
+    cursor: null,
+  });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "ac32e0ate3o6d71438fbfb04d2cec6d9";
+  let context =
+    "You are a recipes expert and loves to show recent and short recipes. Your mission is to generate the title, the list of ingredients and the steps to do the recipe but each line has to be separate from each other with a <br />, the title has to be in a <h3></h3> element. The steps and the ingredients have to be in a <h4></h4> element. Make sure to follow the user instructions.";
+  let prompt = `User instructions: Generate a recipe about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
